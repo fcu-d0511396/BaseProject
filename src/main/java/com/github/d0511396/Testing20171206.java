46e1da81abe97a2b0238340ed48abc27f5bfbf92
d0511396 <chance4447@gmail.com>
2017-12-06 12:01:51 +0800

    995

---
+++ src/main/java/com/github/d0511396/Testing20171206.java
@@ -0,0 +1,18 @@
+package com.github.d0511396;
+
+public class Testing20171206 {
+    public boolean Myfunction(int input){
+        if(input == 0){
+            System.out.println("You enter a zero number.");
+            return false;
+        }
+        else {
+            System.out.println("You enter a number is not zero");
+            return true;
+        }
+    }
+
+    public static void main(String args[]){
+
+    }
+}
---
+++ src/test/java/com/github/d0511396/Testing20171206Test.java
@@ -0,0 +1,16 @@
+package com.github.d0511396;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Testing20171206Test {
+    @Test
+    public void myfunction() throws Exception {
+        Testing20171206 object = new Testing20171206();
+
+        object.Myfunction(0);
+        object.Myfunction(12);
+    }
+
+}
